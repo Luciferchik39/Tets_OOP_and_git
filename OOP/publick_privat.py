5a7a02c377fdec2eec5a3c02f150ad68dcdeccdb
Ilya <ilya@ops.com>
2026-02-14 17:39:41 +0200

    достал фаил и ветки oop_work, поправил gitignore

--- OOP/publick_privat.py
+++ OOP/publick_privat.py
@@ -5,105 +5,105 @@ protected _ prvate __
 """
 
 
-# class Banc_acc:
-#     def __init__(self, name, balans, passport):
-#         self.__name = name  # Приватный атрибут
-#         self.__balans = balans  # Приватный атрибут
-#         self.__pasport = passport  # Приватный атрибут
-#
-#     def print_privat(self):
-#         """Публичный метод, который может обращаться к приватным атрибутам"""
-#         return f'{self.__name}, {self.__balans}, {self.__pasport}'
-#
-#
-# # Создаем экземпляр
-# acc_ilya = Banc_acc('Ilya', 250000, 1814252728)
-#
-# # 1. Прямой доступ к приватным атрибутам НЕ работает
-# # print(acc_ilya.__name)  # AttributeError
-# # print(acc_ilya.__balans)  # AttributeError
-# # print(acc_ilya.__pasport)  # AttributeError
-#
-# # 2. Доступ через публичный метод работает
-# print(acc_ilya.print_privat())  # Ilya, 250000, 1814252728
-#
-# # 3. Доступ через name mangling работает (но так делать НЕ нужно!)
-# print(acc_ilya._Banc_acc__name)  # Ilya
-# print(acc_ilya._Banc_acc__balans)  # 250000
-# print(acc_ilya._Banc_acc__pasport)  # 1814252728
-#
-# """
-# property, getter-методы, setter-методы
-# """
-#
-# class Banc_acc:
-#     def __init__(self, name, balans, passport):
-#         self.__name = name  # Приватный атрибут
-#         self.__balans = balans  # Приватный атрибут
-#         self.__pasport = passport  # Приватный атрибут
-#
-#     def public_print(self):
-#         """метод предоставления данных для экземпляра класса через вызов приватной функции"""
-#         return f'{self.__print_privat()}'
-#
-#     def __print_privat(self):
-#         """метод, который обращается к приватным атрибутам"""
-#         return f'{self.__name}, {self.__balans}, {self.__pasport}'
-#
-#     def get_balans(self):
-#         """вывод баланса"""
-#         return f'{self.__balans}'
-#     def set_balans(self, new_balans):
-#         """изменение баланса"""
-#         if not isinstance(new_balans, int):
-#             raise ValueError('Новый баланс должен быть целым числом')
-#         self.__balans = new_balans
-#         return f'баланс равен {self.__balans}'
-#
-# new_peopl = Banc_acc('Jorik', 100, 1415181910)
-# print(new_peopl.get_balans())
-#
-# class Banc_acc:
-#     def __init__(self, name, balans, passport):
-#         self.__name = name  # Приватный атрибут
-#         self.__balans = balans  # Приватный атрибут
-#         self.__pasport = passport  # Приватный атрибут
-#
-#     def public_print(self):
-#         """метод предоставления данных для экземпляра класса через вызов приватной функции"""
-#         return f'{self.__print_privat()}'
-#
-#     def __print_privat(self):
-#         """метод, который обращается к приватным атрибутам"""
-#         return f'{self.__name}, {self.__balans}, {self.__pasport}'
-#
-#     def get_balans(self):
-#         """вывод баланса"""
-#         return f'{self.__balans}'
-#     def set_balans(self, new_balans):
-#         """изменение баланса"""
-#         if not isinstance(new_balans, int):
-#             raise ValueError('Новый баланс должен быть целым числом')
-#         self.__balans = new_balans
-#         return f'баланс равен {self.__balans}'
-#     def del_balans(self):
-#         del self.__balans
-#
-#     balance = property(fget=get_balans, fset=set_balans, fdel=del_balans)
-#
-# a = Banc_acc('Ilya', 250000, 1814252728)
-# c = Banc_acc('Jorik', 100, 1415181910)
-#
-# a.balance = 400
-# print(a.balance)
-# print(c.balance)
-# del a.balance
-# try:
-#     print(a.balance)
-# except Exception as e:
-#     print(f'была ошибка {e}')
-#
-# print(f'вывел {c.balance}')
+class Banc_acc:
+    def __init__(self, name, balans, passport):
+        self.__name = name  # Приватный атрибут
+        self.__balans = balans  # Приватный атрибут
+        self.__pasport = passport  # Приватный атрибут
+
+    def print_privat(self):
+        """Публичный метод, который может обращаться к приватным атрибутам"""
+        return f'{self.__name}, {self.__balans}, {self.__pasport}'
+
+
+# Создаем экземпляр
+acc_ilya = Banc_acc('Ilya', 250000, 1814252728)
+
+# 1. Прямой доступ к приватным атрибутам НЕ работает
+# print(acc_ilya.__name)  # AttributeError
+# print(acc_ilya.__balans)  # AttributeError
+# print(acc_ilya.__pasport)  # AttributeError
+
+# 2. Доступ через публичный метод работает
+print(acc_ilya.print_privat())  # Ilya, 250000, 1814252728
+
+# 3. Доступ через name mangling работает (но так делать НЕ нужно!)
+print(acc_ilya._Banc_acc__name)  # Ilya
+print(acc_ilya._Banc_acc__balans)  # 250000
+print(acc_ilya._Banc_acc__pasport)  # 1814252728
+
+"""
+property, getter-методы, setter-методы
+"""
+
+class Banc_acc:
+    def __init__(self, name, balans, passport):
+        self.__name = name  # Приватный атрибут
+        self.__balans = balans  # Приватный атрибут
+        self.__pasport = passport  # Приватный атрибут
+
+    def public_print(self):
+        """метод предоставления данных для экземпляра класса через вызов приватной функции"""
+        return f'{self.__print_privat()}'
+
+    def __print_privat(self):
+        """метод, который обращается к приватным атрибутам"""
+        return f'{self.__name}, {self.__balans}, {self.__pasport}'
+
+    def get_balans(self):
+        """вывод баланса"""
+        return f'{self.__balans}'
+    def set_balans(self, new_balans):
+        """изменение баланса"""
+        if not isinstance(new_balans, int):
+            raise ValueError('Новый баланс должен быть целым числом')
+        self.__balans = new_balans
+        return f'баланс равен {self.__balans}'
+
+new_peopl = Banc_acc('Jorik', 100, 1415181910)
+print(new_peopl.get_balans())
+
+class Banc_acc:
+    def __init__(self, name, balans, passport):
+        self.__name = name  # Приватный атрибут
+        self.__balans = balans  # Приватный атрибут
+        self.__pasport = passport  # Приватный атрибут
+
+    def public_print(self):
+        """метод предоставления данных для экземпляра класса через вызов приватной функции"""
+        return f'{self.__print_privat()}'
+
+    def __print_privat(self):
+        """метод, который обращается к приватным атрибутам"""
+        return f'{self.__name}, {self.__balans}, {self.__pasport}'
+
+    def get_balans(self):
+        """вывод баланса"""
+        return f'{self.__balans}'
+    def set_balans(self, new_balans):
+        """изменение баланса"""
+        if not isinstance(new_balans, int):
+            raise ValueError('Новый баланс должен быть целым числом')
+        self.__balans = new_balans
+        return f'баланс равен {self.__balans}'
+    def del_balans(self):
+        del self.__balans
+
+    balance = property(fget=get_balans, fset=set_balans, fdel=del_balans)
+
+a = Banc_acc('Ilya', 250000, 1814252728)
+c = Banc_acc('Jorik', 100, 1415181910)
+
+a.balance = 400
+print(a.balance)
+print(c.balance)
+del a.balance
+try:
+    print(a.balance)
+except Exception as e:
+    print(f'была ошибка {e}')
+
+print(f'вывел {c.balance}')
 
 
 class Square:

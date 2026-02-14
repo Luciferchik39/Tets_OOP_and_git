2b08e61fe48ca981e520fb1748418ec30d25af74
Ilya <ilya@ops.com>
2026-02-14 15:03:24 +0200

    познакомился и позанимался с параметризированными декораторами

---
+++ Структуры данных/Связанный список.py
@@ -0,0 +1,29 @@
+
+class Node:
+    def __init__(self, obj):
+        self.obj = obj
+        self.next = None
+
+    def __repr__(self):
+        return f'Объект является узлом'
+
+
+class New_cls:
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        return f'контейнер связного списка'
+
+my_list = New_cls()
+my_list.head = Node(1)
+two = Node(2)
+three = Node(3)
+
+print(my_list.head)
+print(my_list.head.obj)
+print(two.obj)
+print(three.obj)
+print(str(my_list))
+
+
